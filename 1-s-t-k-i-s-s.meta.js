829de48c0e0435e585418981bce6f1ecc3d11e81
djy6y93b8e <112946670+djy6y93b8e@users.noreply.github.com>
2023-07-24 08:29:17 +0000

    commit message

---
+++ 1-s-t-k-i-s-s.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         1-s-t-k-i-s-s
+// @namespace    https://github.com/djy6y93b8e/hoa_danh_la_2/raw/main/1-s-t-k-i-s-s.user.js
+// @version      2023.07.24
+// @description  down 1-s-t-k-i-s-s
+// @author       You
+// @match        https://1stkissmanga.me/manga/*/*/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=1stkissmanga.me
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @grant        none
+// @downloadURL  https://github.com/djy6y93b8e/hoa_danh_la_2/raw/main/1-s-t-k-i-s-s.user.js
+// @updateURL    https://github.com/djy6y93b8e/hoa_danh_la_2/raw/main/1-s-t-k-i-s-s.meta.js
+// ==/UserScript==
